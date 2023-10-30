e7bcf2baf1c30731b5c7f4253653403a8ccd4bdb
Vladislav <vdronovvladislav@gmail.com>
2023-10-30 23:54:13 +0300

    Сделал первый парсес

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,29 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['http://peps.python.org/']
+
+    def parse(self, response):
+        all_links = response.css('a.pep.reference.internal::attr(href)')
+        for pep_link in all_links:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        text = response.css('h1.page-title::text').get()
+        number, name = map(
+            lambda x: x.encode('latin1').decode('unicode_escape'),
+            text.split(' – ', 1)
+        )
+        data = {
+            'number': number.split(' ')[1],
+            'name': name,
+            'status': response.css(
+                'dt:contains("Status") + dd abbr::text'
+            ).get()
+        }
+        yield PepParseItem(data)
